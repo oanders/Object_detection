d9a92b18b4eeb9990d6afb8750d2457f543e581e
Oskar Andersson <oanders@kth.se>
2016-03-28 18:53:14 +0200

    refactoring complete

--- detection.py
+++ detection.py
@@ -16,9 +16,38 @@ FOLDER2 = 'Images/panter/'
 FOLDER3 = 'Images/flaska/'
 FOLDER4 = 'Images/kontroll/'
 
-
+#Main method, it calls other functions and
+#tests different detection algortithms on images
 def main():
     #Ask wich object we are running tests on
+    folder = choose_folder()
+    tests = read_folders(folder)
+    test = choose_test(tests)
+    #Path for a result directory to be created
+    directory = 'results/' + folder + '/' + test
+    #Path for Training image
+    img1 =  folder + test + '/' + 'tr.jpg'
+    print('using training image: ' + img1)
+    tr_img, tr_grey = load__greyScale(img1)
+
+    #Loop through test images
+    nr = read_nr_images(folder + test)
+    i = 1
+    while i < nr:
+        img2 = folder + test + '/' + 't' + str(i) + '.jpg'
+        print('using test image: ' + img2)
+        test_img, test_grey = load__greyScale(img2)
+        #Run algorithms
+        res_sift_img, good_matches_sift, res_akaze_img, good_matches_akaze, res_orb_img, good_matches_orb = run_test_algorithms(tr_grey, test_grey, tr_img, test_img)
+        #Table containing the number of matches for each algorithm
+        table_img = create_table(res_orb_img, good_matches_sift, good_matches_akaze, good_matches_orb)
+        #Draw plots for the resulting images
+        draw_plots(res_sift_img, res_akaze_img, res_orb_img, table_img, i, directory)
+        #go to next image in the folder
+        i = i+1
+
+#Method that goes to a desired folder
+def choose_folder():
     choice = raw_input('choose folder skidsko , panter, flaska, kontroll: ')
 
     if choice == 'skridsko':
@@ -32,76 +61,66 @@ def main():
     else:
         print('Invalid choice')
         return main()
-    names = read_folders(folder)
-
-    print('Available test: \n')
-    print(names)
-    choice2 = raw_input('\n choose test: ')
-    directory = 'results/' + choice + '/' + choice2
-
-
-    img1 =  folder + choice2 + '/' + 'tr.jpg'
-    print('using training image: ' + img1)
-    tr_img, tr_grey = load__greyScale(img1)
-    #Loop through test images
-    nr = read_nr_images(folder + choice2)
-    i = 1
-    while i < nr:
-        img2 = folder + choice2 + '/' + 't' + str(i) + '.jpg'
-        print('using test image: ' + img2)
-        test_img, test_grey = load__greyScale(img2)
-
-        #Call Sift class
-        sift = Sift()
-        kpAS, descAS, kpBS, descBS, good_matches_sift = sift.match(tr_grey, test_grey)
-        #Call Akaze class
-        akaze = AKaze()
-        kpAK, descAK, kpBK, descBK, good_matches_akaze = akaze.match(tr_grey, test_grey)
-        #Call ORB class
-        orb = ORB()
-        kpAorb, descAorb, kpBorb, descBorb, good_matches_orb = orb.match(tr_grey, test_grey)
-
-        #Mask for all three algorithm
-        maskS, dtsS = location_extraction(kpAS, kpBS, good_matches_sift,tr_img)
-        maskK, dtsK = location_extraction(kpAK, kpBK, good_matches_akaze,tr_img)
-        maskorb, dtsorb = location_extraction(kpAorb, kpBorb, good_matches_orb,tr_img)
-
-
-        #Copy of image so that lines from first method do not last to second.
-        tmp_img1 = copy.copy(test_img)
-        tmp_img2 = copy.copy(test_img)
-        res_sift_img = create_results(tr_img, test_img, kpAS, kpBS, dtsS,
-                                            maskS, good_matches_sift)
-        res_akaze_img = create_results(tr_img, tmp_img1, kpAK, kpBK, dtsK,
-                                            maskK, good_matches_akaze)
-
-        res_orb_img = create_results(tr_img, tmp_img2, kpAorb, kpBorb, dtsorb,
-                                            maskorb, good_matches_orb)
-
-        #Create a table with the number of matches for each algorithm
-        table_img = create_table(res_orb_img, good_matches_sift, good_matches_akaze, good_matches_orb)
-        #Draw plots for the resulting images
-        draw_plots(res_sift_img, res_akaze_img, res_orb_img, table_img, i, directory)
-
-        i = i+1
-
+    return folder
 
 #Read a folder containing images and return a list of urls
 def read_folders(path):
-
-    #List of named of all training images
+    print(path)
+    #List of names of all training images
     names = [f for f in os.listdir(path)]
     return names
 
+#choses a folder with one test
+def choose_test(url):
+    print('Available test: \n')
+    print(url)
+    chosen_test = raw_input('\n choose test: ')
+
+    return chosen_test
+
+
+#Returns the number of images of the given path/folder
 def read_nr_images(path):
     images = [im for im in os.listdir(path) if os.path.isfile(os.path.join(path, im))]
     return len(images)
 
 #Read an image and return its grey picture
-def load__greyScale(picture):
-    img = cv2.imread(picture)
+def load__greyScale(image):
+    img = cv2.imread(image)
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img, grey
+
+
+#Runs all 3 algorithms on the same object and test.
+#Returns 1 image for the result of each algorithm tested.
+def run_test_algorithms(tr_grey, test_grey, tr_img, test_img):
+    #Call Sift class
+    sift = Sift()
+    kpAS, descAS, kpBS, descBS, good_matches_sift = sift.match(tr_grey, test_grey)
+    #Call Akaze class
+    akaze = AKaze()
+    kpAK, descAK, kpBK, descBK, good_matches_akaze = akaze.match(tr_grey, test_grey)
+    #Call ORB class
+    orb = ORB()
+    kpAorb, descAorb, kpBorb, descBorb, good_matches_orb = orb.match(tr_grey, test_grey)
+
+    #Mask for all three algorithms
+    maskS, dtsS = location_extraction(kpAS, kpBS, good_matches_sift,tr_img)
+    maskK, dtsK = location_extraction(kpAK, kpBK, good_matches_akaze,tr_img)
+    maskorb, dtsorb = location_extraction(kpAorb, kpBorb, good_matches_orb,tr_img)
+
+    #Copy of image so that lines from first method do not last to second.
+    tmp_img1 = copy.copy(test_img)
+    tmp_img2 = copy.copy(test_img)
+    res_sift_img = create_results(tr_img, test_img, kpAS, kpBS, dtsS,
+                                        maskS, good_matches_sift)
+    res_akaze_img = create_results(tr_img, tmp_img1, kpAK, kpBK, dtsK,
+                                        maskK, good_matches_akaze)
+
+    res_orb_img = create_results(tr_img, tmp_img2, kpAorb, kpBorb, dtsorb,
+                                        maskorb, good_matches_orb)
+
+    return res_sift_img, good_matches_sift, res_akaze_img, good_matches_akaze, res_orb_img, good_matches_orb
 
 #Takes keypoint descriptor and extracts its location
 def location_extraction(kpA, kpB, good_matches, tr_img):
@@ -150,6 +169,8 @@ def create_results(tr_img, tmp_img, kpA, kpB, dst, matchesMask, good_matches):
 
         return res_img
 
+#Creates a table with the number of matches that each algortihm found
+#for the object.
 def create_table(res_orb_img, good_matches_sift, good_matches_akaze, good_matches_orb):
     h, w, d = res_orb_img.shape
     table_img = np.zeros((h,w,d), np.uint8)
@@ -176,8 +197,5 @@ def draw_plots(sift, akaze, orb, table, index, directory):
     number = index
     fig_name = directory + '/res' + str(number) + '.png'
     plot.savefig(fig_name, format ='png', dpi = 600)
-
-    #plot.show()
-
 
 main()
